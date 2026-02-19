42aff7f18f9bb04af48599af722cecb3a76afd46
Gavin Borne <zentiphdev@gmail.com>
2026-02-19 13:41:56 -0500

    delete default layout

---
+++ app/home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1 className="font-mono">Gavin Borne</h1>
+      <p>Portfolio site (WIP)</p>
+    </main>
+  );
+}
