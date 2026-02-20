5899e7dd227dfc2dab52d2107787b91d06a2cec0
Gavin Borne <zentiphdev@gmail.com>
2026-02-19 19:47:29 -0500

    add links section to homepage (unformatted, missing images)

--- app/pages/home/home.tsx
+++ app/pages/home/home.tsx
@@ -4,6 +4,23 @@ export default function Home() {
   return (
     <main>
       <h1>Gavin Borne</h1>
+      <ul className="icon-links">
+        <li>
+          <a href="/resume.pdf">
+            <img src="/resume.svg" alt="Resume" />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/Zentiph">
+            <img src="/github.svg" alt="GitHub" />
+          </a>
+        </li>
+        <li>
+          <a href="https://linkedin.com/in/gavinborne">
+            <img src="/linkedin.svg" alt="LinkedIn" />
+          </a>
+        </li>
+      </ul>
       <p>Portfolio site (WIP)</p>
     </main>
   );
