32d58c040078e7c2e503d5fb89238a479bfff590
Gavin Borne <zentiphdev@gmail.com>
2026-02-20 12:53:46 -0500

    separate links list into its own component and add gradient effect to background

--- app/pages/home.tsx
+++ app/pages/home.tsx
@@ -1,16 +1,14 @@
-import { EmailIcon } from "@/app/components/icons/email-icon";
-import { GitHubIcon } from "@/app/components/icons/github-icon";
-import { LinkedInIcon } from "@/app/components/icons/linkedin-icon";
-import { ResumeIcon } from "../components/icons/resume-icon";
+import LinksList from "../components/home/links-list";
 
+/**
+ * The homepage component.
+ *
+ * This component renders the homepage content, including the
+ * heading, subheading, and links list.
+ *
+ * @returns A JSX element representing the homepage content.
+ */
 export default function Home() {
-  const iconLinkStyle =
-    "flex flex-col items-center gap-2" +
-    "text-[var(--color-foreground-dim)] " +
-    "transition-[filter,color] duration-250 " +
-    "hover:text-[var(--color-foreground)] " +
-    "hover:[filter:drop-shadow(0_0_4px_currentColor)]";
-
   return (
     <main>
       <h1
@@ -23,52 +21,7 @@ export default function Home() {
         Computer Science Student
       </p>
 
-      <ul className="m-8 flex justify-center gap-6">
-        <li>
-          <a
-            href="https://github.com/Zentiph"
-            className={iconLinkStyle}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <GitHubIcon className="h-12 w-12" />
-            <span className="text-sm">GitHub</span>
-          </a>
-        </li>
-        <li>
-          <a
-            href="/resume.pdf"
-            className={iconLinkStyle}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <ResumeIcon className="h-12 w-12" />
-            <span className="text-sm">Resume</span>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://linkedin.com/in/gavin-borne"
-            className={iconLinkStyle}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <LinkedInIcon className="h-12 w-12" />
-            <span className="text-sm">LinkedIn</span>
-          </a>
-        </li>
-        <li>
-          <a
-            href="mailto:gavin.bor0925@gmail.com"
-            className={iconLinkStyle}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <EmailIcon className="h-12 w-12" />
-            <span className="text-sm">Email</span>
-          </a>
-        </li>
-      </ul>
+      <LinksList className="m-8 flex justify-center gap-6" />
 
       <p className="text-[1.25rem]">The</p>
     </main>
