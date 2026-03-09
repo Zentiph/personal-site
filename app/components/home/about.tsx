7feefd57ae4792bd446df2f121e1816501c40eb2
Gavin Borne <gavin.bor0925@gmail.com>
2026-03-09 18:49:37 -0400

    fix badge wrapping

--- app/components/home/about.tsx
+++ app/components/home/about.tsx
@@ -15,20 +15,21 @@ export default function About({ className }: { className?: string }) {
       <h2 className="m-5 mt-50 font-mono text-5xl text-center glow-1">About</h2>
 
       <p className={p_style}>
-        Hi there! I'm <strong>Gavin</strong>, and I'm a computer science student at RIT,
-        though I started coding long before that. I've primarily focused on{" "}
-        <strong>back-end</strong> development, but I enjoy <strong>front-end</strong>{" "}
-        and <strong>full-stack</strong> development as well. I've worked with a wide
-        range of languages and tools, and I'm always eager to learn new ones.
+        Hi there! I'm <strong>Gavin</strong>, and I'm a second year computer science
+        student at RIT, though I started programming long before my enrollment. I've
+        primarily focused on <strong>back-end</strong> development, but I enjoy{" "}
+        <strong>front-end</strong> and <strong>full-stack</strong> development as well.
+        I've worked with a wide range of languages and tools, and I'm always eager to
+        learn new ones.
       </p>
 
       <p className={p_style}>
         All of my projects and programming work traces back to a{" "}
         <strong>love of learning</strong> and <strong>exploration</strong>. I enjoy not
         just the technical aspects of programming, but also the creative and
-        architectural aspects of it as well. The task of not just coding but designing
-        the shape and structure of a project, and how individual pieces, when arranged
-        effectively, can come together to build a whole, is what I love.
+        architectural aspects of it as well. The task of not just writing code but
+        designing the shape and structure of a project, and how individual pieces, when
+        arranged effectively, can come together to build a whole, is what I love.
       </p>
 
       <Languages />
--- app/components/home/languages.tsx
+++ app/components/home/languages.tsx
@@ -8,44 +8,42 @@ import BadgeIcon from "../icons/badge-icon";
  * @returns The JSX element representing the component.
  */
 export default function Languages({ className }: { className?: string }) {
-  const ulStyle = "m-5 flex justify-center gap-6";
+  const ulStyle = "m-5 flex flex-wrap justify-center gap-6 max-w-[600px] mx-auto";
+  const liStyle = "min-w-[80px] flex justify-center";
 
   return (
     <section className={className}>
       <h3 className="m-5 mt-20 font-mono text-3xl text-center glow-0.5">Languages</h3>
 
       <ul className={ulStyle}>
-        <li>
+        <li className={liStyle}>
           <BadgeIcon imgSrc="/icons/badges/python.svg" text="Python" />
         </li>
-        <li>
+        <li className={liStyle}>
           <BadgeIcon imgSrc="/icons/badges/java.svg" text="Java" />
         </li>
-        <li>
+        <li className={liStyle}>
           <BadgeIcon imgSrc="/icons/badges/c.svg" text="C" />
         </li>
-        <li>
+        <li className={liStyle}>
           <BadgeIcon imgSrc="/icons/badges/rust.svg" text="Rust" />
         </li>
-        <li>
+        <li className={liStyle}>
           <BadgeIcon imgSrc="/icons/badges/lua.svg" text="Lua" />
         </li>
-        <li>
+        <li className={liStyle}>
           <BadgeIcon imgSrc="/icons/badges/luau.svg" text="Luau" />
         </li>
-      </ul>
-
-      <ul className={ulStyle}>
-        <li>
+        <li className={liStyle}>
           <BadgeIcon imgSrc="/icons/badges/html.svg" text="HTML" />
         </li>
-        <li>
+        <li className={liStyle}>
           <BadgeIcon imgSrc="/icons/badges/css.svg" text="CSS" />
         </li>
-        <li>
+        <li className={liStyle}>
           <BadgeIcon imgSrc="/icons/badges/javascript.svg" text="JavaScript" />
         </li>
-        <li>
+        <li className={liStyle}>
           <BadgeIcon imgSrc="/icons/badges/typescript.svg" text="TypeScript" />
         </li>
       </ul>
