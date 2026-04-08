ebe254dbaca84a1c7a2f84c49d5266f1fba66e9b
Gavin Borne <gavin.bor0925@gmail.com>
2026-04-08 13:51:38 -0400

    make navbar glow on hover

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,21 +1,37 @@
 export default function Navbar() {
+  const aStyle =
+    "text-[var(--color-foreground-dim)] " +
+    "transition-[filter,color] duration-200 " +
+    "hover:text-[var(--color-foreground)] " +
+    "hover:glow-1";
+
   return (
-    <nav className="flex justify-center">
-      <ul className="m-5 flex flex-wrap justify-center gap-6 max-w-[600px] mx-auto">
+    <nav className="flex justify-center text-3xl font-mono">
+      <ul className="m-5 flex flex-wrap justify-center gap-6 mx-auto">
         <li>
-          <a href="#home">Home</a>
+          <a href="#home" className={aStyle}>
+            Home
+          </a>
         </li>
         <li>
-          <a href="#about">About</a>
+          <a href="#about" className={aStyle}>
+            About
+          </a>
         </li>
         <li>
-          <a href="#projects">Projects</a>
+          <a href="#projects" className={aStyle}>
+            Projects
+          </a>
         </li>
         <li>
-          <a href="#skills">Skills</a>
+          <a href="#skills" className={aStyle}>
+            Skills
+          </a>
         </li>
         <li>
-          <a href="#contact">Contact</a>
+          <a href="#contact" className={aStyle}>
+            Contact
+          </a>
         </li>
       </ul>
     </nav>
